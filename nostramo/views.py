b864f3c0c3f250ba64dce123bb4bd422a3d48844
Griffin Evans <griffinevans@protonmail.com>
2025-01-11 22:02:07 -0800

    streamline

--- nostramo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-
-def index(request):
-    return render(request, 'nostramo/index.html')
-def about(request):
-    return render(request, 'nostramo/about.html')
-def sitemap(request):
-    return render(request, 'nostramo/sitemap.html')
